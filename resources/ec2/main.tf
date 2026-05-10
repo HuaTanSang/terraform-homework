e124abeb2c517ae037b8ed483749d8d6c41b5047
HuaTanSang <huatansang2004@gmail.com>
2026-05-10 15:23:37 +0700

    feat: add ec2 creation

---
+++ resources/ec2/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = var.region
+  access_key                  = var.access_key
+  secret_key                  = var.secret_key
+
+  # Skip validation due to Ministack 
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+
+  endpoints {
+    ec2 = var.endpoint_url
+  }
+}
+
+# Initialize the EC2 instance resource
+resource "aws_instance" "my_local_ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "My-LocalStack-EC2"
+  }
+}
---
+++ resources/ec2/variables.tf
@@ -0,0 +1,37 @@
+variable "region" {
+  description = "The AWS region to deploy the EC2 instance"
+  type        = string
+  default     = "us-east-1"
+}
+
+### EC2 variable
+variable "instance_type" {
+  description = "The type of EC2 instance to create"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
+  default     = "ami-000000000001" 
+}
+
+
+variable "access_key" {
+  description = "AWS Access Key for LocalStack"
+  type        = string
+  default     = "test"
+}
+
+variable "secret_key" {
+  description = "AWS Secret Key for LocalStack"
+  type        = string
+  default     = "test"
+}
+
+variable "endpoint_url" {
+  description = "The endpoint URL for LocalStack"
+  type        = string
+  default     = "http://localhost:4566"
+}
